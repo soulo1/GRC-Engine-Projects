f68b775c76e3bc32737240dd4cf0fd865b86d61f
Sean D <154912857+soulo1@users.noreply.github.com>
2026-07-08 19:29:37 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -4,7 +4,7 @@ variable "region" {
   default     = "us-east-1"
 }
 
-variable "project_name" {
+variable "grc-challenge" {
   type        = string
   description = "Short project identifier. Becomes part of bucket names and the Project tag."
   validation {
@@ -13,7 +13,7 @@ variable "project_name" {
   }
 }
 
-variable "environment" {
+variable "dev" {
   type        = string
   description = "Deployment environment. Drives the Environment tag."
   validation {
